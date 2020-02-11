97b9fc77aa0385ebd6874fc2908d418d1b5f50e7
Christine Stone <christinestone932@gmail.com>
2020-02-11 22:01:46 +1100

    Updates

---
+++ precache-manifest.80de4491ce69a4517d8c1328eecad917.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b5a1a149901f6a6cf6d814aea3f16a5",
+    "url": "/react-spotify/index.html"
+  },
+  {
+    "revision": "c69a81a86bc73c52b9da",
+    "url": "/react-spotify/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "1bac1c1d1c26a21c2479",
+    "url": "/react-spotify/static/css/main.f3be2e04.chunk.css"
+  },
+  {
+    "revision": "c69a81a86bc73c52b9da",
+    "url": "/react-spotify/static/js/2.468b44e3.chunk.js"
+  },
+  {
+    "revision": "3cddaf6395ecd7bc0fec7e3043a4b18e",
+    "url": "/react-spotify/static/js/2.468b44e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bac1c1d1c26a21c2479",
+    "url": "/react-spotify/static/js/main.f395133d.chunk.js"
+  },
+  {
+    "revision": "b8d1b16a4ff39ec4c5ba",
+    "url": "/react-spotify/static/js/runtime-main.87859495.js"
+  }
+]);
