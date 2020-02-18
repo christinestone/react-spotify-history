f39f295ee4cde95b4de02912beeb20faf177679b
Christine Stone <christinestone932@gmail.com>
2020-02-18 21:01:04 +1100

    Updates

---
+++ precache-manifest.e08478ad8bd8135da595730a6c8ce4c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed22979557d2b2e7cbf072d0c25ea4c6",
+    "url": "/spotify-history-ui/index.html"
+  },
+  {
+    "revision": "3bb9839faeb2b03298bb",
+    "url": "/spotify-history-ui/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "694840980ccab6c35f45",
+    "url": "/spotify-history-ui/static/css/main.f3be2e04.chunk.css"
+  },
+  {
+    "revision": "3bb9839faeb2b03298bb",
+    "url": "/spotify-history-ui/static/js/2.bf4ed56f.chunk.js"
+  },
+  {
+    "revision": "3cddaf6395ecd7bc0fec7e3043a4b18e",
+    "url": "/spotify-history-ui/static/js/2.bf4ed56f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "694840980ccab6c35f45",
+    "url": "/spotify-history-ui/static/js/main.87cbabf6.chunk.js"
+  },
+  {
+    "revision": "5e4d1f9c914e312011f4",
+    "url": "/spotify-history-ui/static/js/runtime-main.1ae4a096.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-spotify/precache-manifest.80de4491ce69a4517d8c1328eecad917.js"
+  "/spotify-history-ui/precache-manifest.e08478ad8bd8135da595730a6c8ce4c1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-spotify/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/spotify-history-ui/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
